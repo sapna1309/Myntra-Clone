27b868693ddfabd9faa4c0e900621b3309321525
akash <akashfulari007@gmail.com>
2023-01-18 22:09:27 +0530

    mens path updated

--- src/Redux/Product/Product.api.js
+++ src/Redux/Product/Product.api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const getMensProductsAPI = async() =>{
-    let res = await axios.get(`http://localhost:3000/MensData`)
+    let res = await axios.get(`https://classic-world.onrender.com/MensData`)
     return res.data
 }
