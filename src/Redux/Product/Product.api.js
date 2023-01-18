66ac6fb3702c95d7383c56d649770bd659527408
akash <akashfulari007@gmail.com>
2023-01-18 22:14:55 +0530

    result

--- src/Redux/Product/Product.api.js
+++ src/Redux/Product/Product.api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const getMensProductsAPI = async() =>{
-    let res = await axios.get(`https://classic-world.onrender.com/MensData`)
-    return res.data
+    let result = await axios.get(`https://classic-world.onrender.com/MensData`)
+    return result.data
 }
