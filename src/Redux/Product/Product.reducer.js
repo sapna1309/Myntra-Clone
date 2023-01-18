a3f6bc6fe3e8498511dc742ded42fffe318e3422
akash <akashfulari007@gmail.com>
2023-01-18 21:17:40 +0530

    redux and mens product page

---
+++ src/Redux/Product/Product.reducer.js
@@ -0,0 +1,42 @@
+import * as types from "./Product.type"
+
+let initialState={
+    loading:false,
+    error:false,
+    products:[]
+    
+}
+
+export const mensProductReducer = (state =initialState , {type , payload})=>{
+    switch(type){
+        case types.GET_PRODUCTS_LOADING:{ 
+         return {
+               ...state,
+               loading:true,
+               error:false
+            }
+        }
+        case types.GET_PRODUCTS_SUCCESS:{ 
+         return {
+               ...state,
+               loading:false,
+               error:false,
+               products:payload
+               
+            }
+        }
+        case types.GET_PRODUCTS_ERROR:{ 
+         return {
+               ...state,
+               loading:false,
+               error:true
+            }
+        }
+
+
+        default : {
+            return state
+        }
+    }
+
+}
