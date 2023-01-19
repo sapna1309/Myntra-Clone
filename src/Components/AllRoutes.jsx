acecff1f11738b77455d7e3c83924e77b4af0040
Sapna Sharma <sapna.sharma220516@gmail.com>
2023-01-19 22:24:24 +0530

    admin-ui

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -7,11 +7,11 @@ import PageNotFound from '../Pages/PageNotFound'
 import Register from '../Pages/Register'
 
 import Product from '../Pages/Product'
- fp06_174_Day_3
+ 
 import AdminDashboard from '../Pages/Admin/Admin-Dashboard'
 
 import SingleProductPage from './SingleProductPage'
- main
+ 
 
 const AllRoutes = ({userName}) => {
   console.log(userName)
