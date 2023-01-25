97735a1cf323a8a5db90ea1f5eb6ebc6cda5b969
Sapna Sharma <sapna.sharma220516@gmail.com>
2023-01-25 12:03:55 +0530

    footer

--- src/Components/FinalFooter.jsx
+++ src/Components/FinalFooter.jsx
@@ -1,49 +1,43 @@
-
 import {
   Box,
   Container,
   Stack,
-  SimpleGrid,
   Text,
-  Link,
   VisuallyHidden,
   chakra,
   useColorModeValue,
   Image,
   HStack,
-  Grid
-} from '@chakra-ui/react';
-import { FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
+  Grid,
+} from "@chakra-ui/react";
+import { FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
 
 const ListHeader = ({ children }) => {
   return (
-    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
       {children}
     </Text>
   );
 };
 
-const SocialButton = ({
-  children,
-  label,
-  href,
-}) => {
+const SocialButton = ({ children, label, href }) => {
   return (
     <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
       w={8}
       h={8}
-      cursor={'pointer'}
-      as={'a'}
+      cursor={"pointer"}
+      as={"a"}
       href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
       _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}>
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
       <VisuallyHidden>{label}</VisuallyHidden>
       {children}
     </chakra.button>
@@ -53,106 +47,133 @@ const SocialButton = ({
 export default function FinalFooter() {
   return (
     <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
-      <Container as={Stack} maxW={'6xl'} py={10}>
-        <Grid gridTemplateColumns={{ base:"repeat(1,1fr)", sm: "repeat(2,1fr)", md: "repeat(3,1fr)",lg:"repeat(4,1fr)"}} gap={5} >
-          <Stack align={'flex-start'}>
-            <ListHeader>ONLINE SHOPPING</ListHeader>
-            <Link href={"#"}>Men</Link>
-              <Link href={"#"}>Women</Link>
-              <Link href={"#"}>Kids</Link>
-              <Link href={"#"}>Beauty</Link>
-              <Link href={"#"}>Gift card</Link>
-              <Link>Classic World Insider</Link>
-            <Link href={'#'}>About Us</Link>
-            <Link href={'#'}>Blog</Link>
-            <Link href={'#'}>Careers</Link>
-            <Link href={'#'}>Contact Us</Link>
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container as={Stack} maxW={"7xl"} py={10} m={'auto'} border={'0px solid black'}>
+        <Grid
+          gridTemplateColumns={{
+            base: "repeat(1,1fr)",
+            sm: "repeat(2,1fr)",
+            md: "repeat(3,1fr)",
+            lg: "repeat(4,1fr)",
+          }}
+          border={'0px solid red'}
+          gap={'50px'}
+          justifyContent={'space-evenly'}
+          fontSize={[15, 15, 15, 15]}
+        >
+          <Stack align={"flex-start"} boxShadow={'sm'} textAlign={'left'} border={'0px solid black'}>
+            <Text fontWeight={'bold'} fontSize={15} noOfLines={1}>ONLINE SHOPPING</Text>
+            <Text noOfLines={1}>Men</Text>
+            <Text noOfLines={1}>Women</Text>
+            <Text noOfLines={1}>Kids</Text>
+            <Text noOfLines={1}>Beauty</Text>
+            <Text noOfLines={1}>Gift card</Text>
+            <Text noOfLines={1}>Classic World Insider</Text>
+            <Text noOfLines={1}>About Us</Text>
+            <Text noOfLines={1}>Blog</Text>
+            <Text noOfLines={1}>Careers</Text>
+            <Text noOfLines={1}>Contact Us</Text>
           </Stack>
 
-          <Stack align={'flex-start'}>
-            <ListHeader>USEFUL LINKS</ListHeader>
-            <Link href={"#"}>Blog</Link>
-              <Link href={"#"}>Careers</Link>
-              <Link href={"#"}>Site Map</Link>
-              <Link href={"#"}>Corporate Information</Link>
-              <Link href={"#"}>Whitehat</Link>
-              <Link href={'#'}>Help Center</Link>
-            <Link href={'#'}>Safety Center</Link>
-            <Link href={'#'}>Community Guidelines</Link>
-            <Link href={'#'}>Safety Center</Link>
-            <Link href={'#'}>Community Guidelines</Link>
+          <Stack align={"flex-start"} boxShadow={'sm'}textAlign={'left'} border={'0px solid black'}>
+            <Text fontWeight={'bold'} fontSize={15} noOfLines={1}>USEFUL LINKS</Text>
+            <Text noOfLines={1}>Blog</Text>
+            <Text noOfLines={1}>Careers</Text>
+            <Text noOfLines={1}>Site Map</Text>
+            <Text noOfLines={1}>Corporate Information</Text>
+            <Text noOfLines={1}>Whitehat</Text>
+            <Text noOfLines={1}>Help Center</Text>
+            <Text noOfLines={1}>Safety Center</Text>
+            <Text noOfLines={1}>Community Guidelines</Text>
+            <Text noOfLines={1}>Safety Center</Text>
+            <Text noOfLines={1}>Community Guidelines</Text>
           </Stack>
 
-          <Stack align={'flex-start'}>
-            <ListHeader>CUSTOMER POLICIES</ListHeader>
-            <Link href={"#"}>Contact Us</Link>
-            <Link href={"#"}>FAQ</Link>
-            <Link href={"#"}>T&C</Link>
-            <Link href={"#"}>Terms Of Use</Link>
-            <Link href={"#"}>Track Orders</Link>
-            <Link href={"#"}>Shipping</Link>
-            <Link href={"#"}>Cancellation</Link>
-            <Link href={"#"}>Return</Link>
-            <Link href={"#"}>Privacy Policy</Link>
-            <Link href={"#"}>Grievance Officer</Link>
+          <Stack align={"flex-start"}boxShadow={'sm'} textAlign={'left'} border={'0px solid black'}>
+            <Text fontWeight={'bold'} fontSize={15} noOfLines={1}>CUSTOMER POLICIES</Text>
+            <Text noOfLines={1}>Contact Us</Text>
+            <Text noOfLines={1}>FAQ</Text>
+            <Text noOfLines={1}>T&C</Text>
+            <Text noOfLines={1}>Terms Of Use</Text>
+            <Text noOfLines={1}>Track Orders</Text>
+            <Text noOfLines={1}>Shipping</Text>
+            <Text noOfLines={1}>Cancellation</Text>
+            <Text noOfLines={1}>Return</Text>
+            <Text noOfLines={1}>Privacy Policy</Text>
+            <Text noOfLines={1}>Grievance Officer</Text>
           </Stack>
 
-          <Stack align={'flex-start'}>
-            <ListHeader>REGISTER OFFICE ADDRESS</ListHeader>
-            <Link href={"#"}>Begonia and Clover situated,</Link>
-            <Link href={"#"}>BegEmbassy Tech Village,</Link>
-            <Link href={"#"}>Outer Ring Road,</Link>
-            <Link href={"#"}>Devarabeesanahalli Village,</Link>
-            <Link href={"#"}>Varthur Hobli,</Link>
-            <Link href={"#"}>Bengaluru – 560103, India</Link>
-            <Link href={"#"}>CIN: U72300KA2007PTC041799</Link>
-            <Link href={"#"}>Telephone: +91-80-61561999</Link>
+          <Stack alignItems={"flex-start"} boxShadow={'sm'}textAlign={'left'}border={'0px solid black'}  >
+            <Text fontWeight={'bold'} fontSize={15} noOfLines={1}>REGISTER OFFICE ADDRESS</Text>
+            <Text noOfLines={1}>Begonia and Clover situated,</Text>
+            <Text noOfLines={1}>BegEmbassy Tech Village,</Text>
+            <Text noOfLines={1}>Outer Ring Road,</Text>
+            <Text noOfLines={1}>Devarabeesanahalli Village,</Text>
+            <Text noOfLines={1}>Varthur Hobli,</Text>
+            <Text noOfLines={1}>Bengaluru – 560103, India</Text>
+            <Text noOfLines={1}>CIN: U72300KA2007PTC041799</Text>
+            <Text noOfLines={1}>Telephone: +91-80-61561999</Text>
           </Stack>
 
-          <Stack align={'flex-start'}>
+          <Stack align={"flex-start"}>
             <ListHeader>Install App</ListHeader>
-            
-            <Image src='https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg' alt='playStore' width={['50%','50%','80%','100%']} />
-            <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Available_on_the_App_Store_%28black%29_SVG.svg/203px-Available_on_the_App_Store_%28black%29_SVG.svg.png?20130522010834' alt='applestore' 
-             width={['50%','50%','80%','100%']}  />
-           
+
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+              alt="playStore"
+              width={["40%", "40%", "60%", "80%"]}
+            />
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Available_on_the_App_Store_%28black%29_SVG.svg/203px-Available_on_the_App_Store_%28black%29_SVG.svg.png?20130522010834"
+              alt="applestore"
+              width={["40%", "40%", "60%", "80%"]}
+            />
           </Stack>
 
-          <Stack align={'flex-start'}>
+          <Stack align={"flex-start"}>
             <ListHeader>Accurance</ListHeader>
             <HStack>
-            <Image src='https://previews.123rf.com/images/arcady31/arcady311304/arcady31130400034/19245976-original-stamp.jpg' alt='playStore' width={['30%','30%','40%','50%']} />
-            
-            <Image src='https://ocxooscillator.com/wp-content/uploads/2020/06/30days_Return_OcxoOscillator-1.png' alt='applestore' 
-             width={['35%','35%','45%','60%']}  />
-           </HStack>
+              <Image
+                src="https://previews.123rf.com/images/arcady31/arcady311304/arcady31130400034/19245976-original-stamp.jpg"
+                alt="playStore"
+                width={["26%", "26%", "38%", "45%"]}
+              />
+
+              <Image
+                src="https://ocxooscillator.com/wp-content/uploads/2020/06/30days_Return_OcxoOscillator-1.png"
+                alt="applestore"
+                width={["26%", "26%", "38%", "45%"]}
+              />
+            </HStack>
           </Stack>
         </Grid>
       </Container>
 
       <Box
         borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
+      >
         <Container
           as={Stack}
-          maxW={'6xl'}
+          maxW={"6xl"}
           py={4}
-          direction={{ base: 'column', md: 'row' }}
+          direction={{ base: "column", md: "row" }}
           spacing={4}
-          justify={{ md: 'space-between' }}
-          align={{ md: 'center' }}>
+          justify={{ md: "space-between" }}
+          align={{ md: "center" }}
+        >
           <Text>© 2022 Chakra Templates. All rights reserved</Text>
-          <Stack direction={'row'} spacing={6}>
-            <SocialButton label={'Twitter'} href={'#'}>
+          <Stack direction={"row"} spacing={6}>
+            <SocialButton label={"Twitter"}>
               <FaTwitter />
             </SocialButton>
-            <SocialButton label={'YouTube'} href={'#'}>
+            <SocialButton label={"YouTube"}>
               <FaYoutube />
             </SocialButton>
-            <SocialButton label={'Instagram'} href={'#'}>
+            <SocialButton label={"Instagram"}>
               <FaInstagram />
             </SocialButton>
           </Stack>
