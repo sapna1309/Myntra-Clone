ab3934b1bf4b0f0cdbff2e50dfbfa35ff9449869
Sapna Sharma <sapna.sharma220516@gmail.com>
2023-01-25 08:32:35 +0530

    added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
-import { Box, Image, Center, Grid } from '@chakra-ui/react';
+import { Box, Image, Center, Grid, HStack, Flex } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import axios from 'axios';
 import HomePageHeader from '../assets/HomePageHeader.png';
 import { useEffect } from 'react';
 import FinalNavbar from '../Components/FinalNavbar';
+import FinalFooter from '../Components/FinalFooter';
 //import Footer from '../Components/Footer';
 
 const Home = () => {
@@ -30,38 +31,40 @@ const Home = () => {
           alt='home-page-header' width={"95%"} mt={'60px'} 
           />
            </Center>
+           <Box>
           <Image src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/12/3d8b5917-e115-425b-8c93-2c2e427f9d4f1673517461716-OMG-Deal--2-.jpg' alt='omgDeals' />
-          <Grid gridTemplateColumns={"repeat(10,1fr)"} >
+          <Grid gridTemplateColumns={["repeat(3,1fr)","repeat(6,1fr)","repeat(6,1fr)","repeat(9,1fr)"]} gap={[0,0,0,0]} >
             {data.HomePageOmgDeals?.map((el,i)=>(
               <Image key={i} src={el.image} />
             ))}
           </Grid>
           <Image src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/12/e78ea5f5-8bcc-43e2-afce-529b95a166081673517538108-Grand-Brands.jpg' alt='grandBrands' />
-          <Grid gridTemplateColumns={"repeat(10,1fr)"} >
+          <Grid gridTemplateColumns={["repeat(3,1fr)","repeat(6,1fr)","repeat(6,1fr)","repeat(9,1fr)"]} gap={[0,0,0,0]} >
             {data.HomePageGrandBrands?.map((el,i)=>(
               <Image key={i} src={el.image} />
             ))}
           </Grid>
           <Image src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/12/1c508dcd-ee9f-4485-81a5-71d97bf512e31673517538088-Brands-To-Bag.jpg' alt='brandsToBag' />
-          <Grid gridTemplateColumns={"repeat(9,1fr)"} >
+          <Grid gridTemplateColumns={["repeat(3,1fr)","repeat(6,1fr)","repeat(6,1fr)","repeat(9,1fr)"]} gap={[0,0,0,0]} >
             {data.HomePageBrandsToBag?.map((el,i)=>(
               <Image key={i} src={el.image} />
             ))}
           </Grid>
           <Image src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/11/2eb7efa2-1743-43b4-94c0-8b634d1be9af1673439896772-Budget-Buys--3-.jpg' alt='budgetToBuys' />
-          <Grid gridTemplateColumns={"repeat(8,1fr)"} >
+          <Grid gridTemplateColumns={["repeat(2,1fr)","repeat(4,1fr)","repeat(4,1fr)","repeat(8,1fr)"]} gap={[0,0,0,0]}>
             {data.HomePageBudgetToBuys?.map((el,i)=>(
               <Image key={i} src={el.image} />
             ))}
           </Grid>
           <Image src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/11/ba62da25-d69a-441f-a639-4bd08534f3451673416953973-Shop-By-Category--2-.jpg' alt='shopByCategory' />
-          <Grid gridTemplateColumns={"repeat(6,1fr)"} >
+          <Grid gridTemplateColumns={["repeat(3,1fr)","repeat(6,1fr)","repeat(6,1fr)","repeat(6,1fr)"]} gap={[0,0,0,0]} >
             {data.HomePageShopByCategory
 ?.map((el,i)=>(
               <Image key={i} src={el.image} />
             ))}
           </Grid>
-          {/* <Footer/> */}
+     </Box>
+     <FinalFooter/>
     </Box>
   )
 }
